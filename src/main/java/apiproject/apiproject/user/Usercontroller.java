c0c5587805e5fb3d32945c6be160ed9163513ecd
kongtaesic <xotlr333@naver.com>
2022-09-14 23:39:04 +0900

    회원삭제 API 1

--- src/main/java/apiproject/apiproject/user/Usercontroller.java
+++ src/main/java/apiproject/apiproject/user/Usercontroller.java
@@ -48,4 +48,14 @@ public class Usercontroller {
         return ResponseEntity.created(location).build();
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+    }
+
 }
