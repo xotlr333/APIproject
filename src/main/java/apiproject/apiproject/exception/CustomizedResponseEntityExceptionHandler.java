66147a7e202094c11138778fb1943a5d30deaba5
kongtaesic <xotlr333@naver.com>
2022-09-14 23:11:27 +0900

    예외처러 2

--- src/main/java/apiproject/apiproject/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/apiproject/apiproject/exception/CustomizedResponseEntityExceptionHandler.java
@@ -12,10 +12,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.Date;
 
 @RestController
-@ControllerAdvice //모든 controller 에서 발생하는 exception 를 처리함 (AOP)
+@ControllerAdvice //모든 controller 에서 발생하는 공통적으로 선언되도록 (AOP)
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class )
+    @ExceptionHandler(Exception.class) //이 메소드가 exceptionHandler 로 사용할 수 있음을 지정하는 어노테이션
     public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
         ExceptionResponse exceptionResponse =
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
@@ -23,7 +23,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UserNotFoundException.class )
+    @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundExceptions(Exception ex, WebRequest request){
         ExceptionResponse exceptionResponse =
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
